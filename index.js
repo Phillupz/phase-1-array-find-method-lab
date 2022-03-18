4cc44a9afcb71b9ce7acfd5af36c2de0841aae53
Phillupz <phillup.zukowski@gmail.com>
2022-03-17 21:18:01 -0400

    commit tree 7ec50698a8e912579fc3f445d340032a9f2c9e71

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+  const found = record.find(element => element.result === "W") 
+    if (found) {
+      return (found.year) 
+    }
+  }
+      
+
+
+
